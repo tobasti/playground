0c1b2e59fd317dfad3589f3268f64fba8fd662bf
Tobasti <tobasti@riseup.net>
2017-04-13 11:24:51 +0200

    Add temporary start page to test heroku deploy

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,7 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+
+  def start
+    render html: 'Hello there, this is currently running...'
+  end
 end
