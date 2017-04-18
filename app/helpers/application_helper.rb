b06e4bcee7cd6865dd04144412604b79a8fbb317
Tobasti <tobasti@riseup.net>
2017-04-18 15:10:53 +0200

    Add full_title helper to application

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  BASE_TITLE = "Playground"
+
+  # Returns the full title on a per-page basis
+  def full_title(page_title = '')
+    page_title.empty? ? BASE_TITLE : page_title + " | " + BASE_TITLE
+  end
 end
