ea5a9365ad875ffbcd674e26547eea852b48e166
Tobasti <tobasti@riseup.net>
2017-05-31 13:19:41 +0200

    Finish Signup

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -15,4 +15,17 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div.field_with_errors'
     assert_select 'form[action="/signup"]'
   end
+
+  test "valid user signup" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post signup_path, params: { user: { name: "Claudia",
+                                        email: "claudia@123.de",
+                                        password:              "12345678",
+                                        password_confirmation: "12345678" } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert_not flash.empty?
+  end
 end
